c587d9c41973b5588732fd52d42b69b16786050e
filippbelv <filippbelv1@yandex.ru>
2026-07-26 12:57:31 +0300

    Выполнил_дз_строки

--- 4-expenses/lis_tup.py
+++ 4-expenses/lis_tup.py
@@ -1,5 +1,32 @@
-l = [500, 750, 632, 410, 107, 1050, 200]
-a, b, c = sum(l), max(l), min(l)
-average = a / 7
-t = c, b, a
-print(t)
+s = input().lower().strip()
+
+s = s.replace("рублей", "руб")
+s = s.replace("руб.", "руб")
+s = s.replace("копеек", "коп")
+s = s.replace("коп.", "коп")
+
+parts = s.split()
+
+if len(parts) == 2:
+    if parts[1] == "руб" and parts[0].isdigit():
+        rub = int(parts[0])
+        kop = 0
+        print("{:.2f} ₽".format(rub + kop / 100))
+    else:
+        print("Некорректный формат суммы")
+
+elif len(parts) == 4:
+    if (
+        parts[1] == "руб"
+        and parts[3] == "коп"
+        and parts[0].isdigit()
+        and parts[2].isdigit()
+    ):
+        rub = int(parts[0])
+        kop = int(parts[2])
+        print("{:.2f} ₽".format(rub + kop / 100))
+    else:
+        print("Некорректный формат суммы")
+
+else:
+    print("Некорректный формат суммы")
